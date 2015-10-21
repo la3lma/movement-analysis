aa43a71007f24be938ce1a1cb55bd580f5799ba1
Audun Torp <audun@telenordigital.com>
2015-10-21 22:01:34 +0200

    Take command-line options
    Save and load state

--- src/dataset.py
+++ src/dataset.py
@@ -1,3 +1,5 @@
+'''Usage: dataset.py [--model=x --data=feed] [--plot]
+'''
 import time
 from numpy import fft
 from matplotlib import pyplot as plt
@@ -9,6 +11,8 @@ from sklearn import grid_search
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import confusion_matrix
+from sklearn.externals import joblib
+import docopt
 
 class sample_file:
     def __init__(self, filename):
@@ -56,6 +60,9 @@ class sample_file:
             result.append(segments_buckets)
         return result
         
+    def keep_last_lines(self, num_lines):
+        self.data = self.data[-num_lines:]
+        
 class dataset:
     def __init__(self, foldername, filters = {'dancing': 0, 'walking': 1, 'sitting':2}):
         self.data = []
@@ -81,19 +88,22 @@ def get_samples(foldername, filter=None):
             samples.append(sample)
         
     return samples
-
-
           
 if __name__ == '__main__':
+    arguments = docopt.docopt(__doc__)
     filters = {'dancing': 0, 'walking': 1, 'sitting':2}
-    training = dataset('../datasets/training', filters)
-    
-    svr = svm.SVC()
-    exponential_range = [pow(10, i) for i in range(-2, 2)]
-    parameters = {'kernel':['linear', 'rbf'], 'C':exponential_range, 'gamma':exponential_range}
-    clf = grid_search.GridSearchCV(svr, parameters, n_jobs=8, verbose=True)
-    clf.fit(training.data, training.target)
-    print clf 
+    if arguments['--model']:
+        clf = joblib.load(arguments['--model'])
+    else:
+        training = dataset('../datasets/training', filters)
+        
+        svr = svm.SVC()
+        exponential_range = [pow(10, i) for i in range(-2, 2)]
+        parameters = {'kernel':['linear', 'rbf'], 'C':exponential_range, 'gamma':exponential_range}
+        clf = grid_search.GridSearchCV(svr, parameters, n_jobs=8, verbose=True)
+        clf.fit(training.data, training.target)
+        joblib.dump(clf, '../models/3s_window.pkl')
+        print clf 
 
     validation = dataset('../datasets/validation')
     
@@ -120,13 +130,19 @@ if __name__ == '__main__':
     print "confusion:"
     print(cm)
     
-    # Show confusion matrix in a separate window
-    plt.matshow(cm)
-    plt.title('Confusion matrix')
-    plt.colorbar()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.show()
-
+    if arguments['--plot']:
+        # Show confusion matrix in a separate window
+        plt.matshow(cm)
+        plt.title('Confusion matrix')
+        plt.colorbar()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.show()
     
-    
+    data_feed = arguments['--data']
+    if data_feed:
+        sample = sample_file(data_feed)
+        sample.keep_last_lines(300)
+        samples = sample.get_samples()
+        print clf.predict(samples)
+        
--- src/pca.py
+++
@@ -1,1 +0,0 @@
-from sklearn import datasets
